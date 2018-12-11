cab1149a9f3da344dd1472b3d4c80685abaf8198
AntonieV <antonie.vietor@gmx.de>
2018-12-11 21:54:04 +0100

    neue Testdaten, Python-Script pd fuer Testdatentabelle, snakemake-environment

---
+++ py_scripts/create_table.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+in_data = {'sample': ["a", "b"], 'fq1': ["data/reads/a.chr21.1.fq", "data/reads/b.chr21.1.fq"],
+           'fq2': ["data/reads/a.chr21.2.fq", "data/reads/b.chr21.2.fq"], 'condition': ["treated", "untreated"]}
+
+table_path_of_reads = pd.DataFrame(data=in_data, columns=['sample', 'fq1', 'fq2', 'condition'])
+
+table_path_of_reads.to_csv(path_or_buf="table_path_of_reads", sep='\t', index=True, header=True)
+
+# print(table_path_of_reads)
