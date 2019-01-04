79d2bfe2bf5aec8e50a2ea4560bfc5fd9c7468cd
AntonieV <45459714+AntonieV@users.noreply.github.com>
2019-01-04 22:51:29 +0100

    Bug im sleuth_script.R
    Die Sleuth-Regel  und die Verarbeitung von Input und Output  aus Snakemake funktionieren im sleuth_script.R. Aber Sleuth kann die h5-Dateien nicht korrekt lesen. Von dem Bug wurde schon oefter berichtet und ist wahrscheinlich ein Problem mit den Versionen im env. Ich konnte bisher keine Loesung finden, da sollten wir kommenden Donnerstag mal nachfragen. Im sleuth_script.R stehen jetzt noch verschiedene Tests zum Debuggen, wenn das Script funktioniert, koennen sie dann spaeter geloescht werden.

--- r_scripts/sleuth_script.R
+++ r_scripts/sleuth_script.R
@@ -1,7 +1,9 @@
-###################### Installation ##########################
-###sollte schon ueber das env installiert werden####
+###############################################################
+###################### Installation  ##########################
+        ### wird durch env/sleuth.yaml uebernommen ####
+
 #install.packages("Rtools 3.5")
-#source("http://bioconductor.org/biocLite.R") #insall rhdf5
+#source("http://bioconductor.org/biocLite.R") #install rhdf5
 #biocLite("rhdf5")
 
 #install.packages("devtools")  #package devtools
@@ -16,40 +18,107 @@ suppressMessages({
   library("sleuth")
 })
 
+path <- getwd() # aktuelles Verzeichnis speichern
+
 #Vorbereitungen:
 
-#Verzeichnis-Pfad speichern, in dem die Kallisto-results liegen:
-#sample_id <- dir(file.path("...", "results"))  #hier Pfad zu den Kallisto-results angeben
-
-#Betrachten der in diesem Verzeichnis vorhandenen Kallisto-Dateien:
-#sample_id
-
 #Listen mit den Pfaden zu den einzelnen Kallisto-Dateien anlegen: 
-#kal_dirs <- file.path("...", "results", sample_id, "kallisto")  #hier Pfad zu den Kallisto-results angeben
-kal_dirs <- snakemake@input[["kal_path"]]  # Liste der Pfadnamen der einzelnen Kallisto-results
 
-#Betrachten der Pfad-Liste zu den Kallisto-Dateien:
-#kal_dirs
+kal_dirs <- file.path(gsub(" ","", paste(path, "/", snakemake@input[["kal_path"]])))  # Liste der Pfadnamen der einzelnen Kallisto-results
+
+####################################################################
+####### ZUM DEBUGGEN: verschiedene Varianten der Pfadangaben #######
+
+#kal_dirs <- file.path(gsub(" ","", paste(path, "/", snakemake@input[["kal_path"]],"/"))) 
+#--#kal_dirs <- file.path(gsub(" ","", paste("../", snakemake@input[["kal_path"]])))
+#--#kal_dirs <- file.path(gsub(" ","", paste("../", snakemake@input[["kal_path"]],"/")))
+#kal_dirs <- file.path(gsub(" ","", paste("/",path, "/", snakemake@input[["kal_path"]])))
+#kal_dirs <- file.path(gsub(" ","", paste("/",path, "/", snakemake@input[["kal_path"]],"/")))
+#--#kal_dirs <- file.path(gsub(" ","", paste("/", snakemake@input[["kal_path"]])))
+#--#kal_dirs <- file.path(gsub(" ","", paste("/", snakemake@input[["kal_path"]],"/")))
+#kal_dirs <- file.path(snakemake@input[["kal_path"]])
+
+####################################################################
+
+###############################################################################
+####### ZUM DEBUGGEN: Betrachten der Pfad-Liste zu den Kallisto-Dateien #######
+
+kal_dirs
+
+###############################################################################
+
+
+
+#################################################
+######## ZUM DEBUGGEN: Test der h5-Datei ########
+
+###test_kallisto <- rhdf5::h5read("/home/tharja/Schreibtisch/Projekt_Ursprungsversion/fprdg1/kallisto/a/abundance.h5","/")
+###rhdf5::H5close()
+###test_kallisto$est_counts
+###test_kallisto$aux$ids
+###test_kallisto$aux$length
+
+#################################################
+
 
 #Hilfstabelle anlegen mit Angaben zum experimentellen Design der einzelnen Proben
 #aus dem Kallisto-Verzeichnis:
-#s2c <- read.table(file.path("...", "metadata", "hiseq_info.txt"), #hier Pfad zu den Kallisto-results angeben
+
+#s2c <- read.table(file.path("..", "metadata", "hiseq_info.txt"), 
 #                  header = TRUE, stringsAsFactors=FALSE) 
-s2c <- read.table(snakemake@input[["sam_tab"]])
-s2c <- dplyr::select(s2c, sample = run_accession, condition)
-#s2c
+
+s2c <- read.table(file = snakemake@input[["sam_tab"]] , sep = "\t", 
+                  header = TRUE, stringsAsFactors = FALSE)
+
+s2c <- dplyr::select(s2c, sample, condition)
 
 #Die Pfade der Kallisto-Dateien muessen nun als neue Spalte an die Hilfstabelle angefuegt werden:
 s2c <- dplyr::mutate(s2c, path = kal_dirs)
 
-#Betrachten der Hilfstabelle, auf Korrektheit ueberpruefen:
-#print(s2c)
+
+#######################################################################
+######## ZUM DEBUGGEN: Ausgabe der Hilfstabelle in der Konsole ########
+
+print(s2c)
+
+#######################################################################
+
+
+############################################################################################
+##################### ZUM DEBUGGEN: Einlesen einer tsv- und h5-Datei #######################
+
+cat("\n******************************************************************************\n\n")
+a <- gsub(" ", "", paste(s2c$path[1], "/abundance.tsv"))
+b <- gsub(" ", "", paste(s2c$path[1], "/abundance.h5"))
+x <- read_kallisto_tsv(a)
+str(x)
+cat("\n******************************************************************************\n\n")
+y <- read_kallisto_h5(b, read_bootstrap = FALSE)
+str(y)   
+cat("\n******************************************************************************\n\n")
+
+############################################################################################
+
 
 #Kallisto-Daten in Sleuth laden, anlegen eines Sleuth-Objekts:
 
 #load the kallisto processed data into the object
 #Initialisierung des Sleuth-Objekts:
-so <- sleuth_prep(s2c, extra_bootstrap_summary = TRUE)
+
+
+####################################### ZUM DEBUGGEN #################################################
+#rhdf5::h5read("/home/tharja/Schreibtisch/Projekt_Ursprungsversion/fprdg1/kallisto/a/abundance.h5","/")
+#rhdf5::h5read("/home/tharja/Schreibtisch/Projekt_Ursprungsversion/fprdg1/kallisto/b/abundance.h5","/")
+#rhdf5::h5read("/home/tharja/Schreibtisch/Projekt_Ursprungsversion/fprdg1/kallisto/c/abundance.h5","/")
+#rhdf5::h5read("/home/tharja/Schreibtisch/Projekt_Ursprungsversion/fprdg1/kallisto/d/abundance.h5","/")
+#rhdf5::H5close()
+
+######################################################################################################
+
+
+so <- sleuth_prep(s2c, extra_bootstrap_summary = TRUE) #, read_bootstrap_tpm=TRUE, full_model = ~condition)
+str(so)
+cat("\n******************************************************************************\n\n")
 
 #estimate parameters for the sleuth response error measurement (full) model
 #Das Sleuth-Objekt an das Full-Model anpassen:
@@ -64,17 +133,35 @@ so <- sleuth_fit(so, ~1, 'reduced')
 so <- sleuth_lrt(so, 'reduced', 'full')
 
 #Betrachten des Full-Models und des reduzierten Modells:
-#models(so)
+models(so)
 
 #Betrachen der signifikanten Ergebnisse aus dem Likelihood-Ratio-Test:
 sleuth_table <- sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE)
 sleuth_significant <- dplyr::filter(sleuth_table, qval <= 0.05)
-#head(sleuth_significant, 20)
+head(sleuth_significant, 20)
 
 #### TODO - Gennamen zu den IDs hinzugfuegen
 
 #### TODO - Outputdatei anlegen, ggf. Hilfsdatei zur Ueberpruefung der Zwischenvariablen
 #### auf Korrektheit anlegen
 
-snakemake@output <- sleuth_table
 
+write.table(sleuth_table, file = "sleuth/p-values_all_transcripts.csv", sep = "\t")
+write.table(sleuth_significant, file = "sleuth/significant_transcripts.csv", sep = "\t")
+
+##################################################################################
+######## ZUM DEBUGGEN: Testfiles anlegen ohne Sleuth-Analyse und ausgeben ########
+              ##### zum Testen der Sluth-Regel in Snakemake #####
+
+#write.table(s2c, file = "sleuth/p-values_all_transcripts.csv", sep = "\t")
+#write.table(s2c, file = "sleuth/significant_transcripts.csv", sep = "\t")
+
+#y <- read.table(file = "sleuth/p-values_all_transcripts.csv", sep = "\t", header = TRUE)
+#z <- read.table(file = "sleuth/significant_transcripts.csv", sep = "\t", header = TRUE)
+#y
+#z
+
+##################################################################################
+
+
+#snakemake@output <- list(file.path(y))
