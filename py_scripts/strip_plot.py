58e0958b85f3cdc1c2b3d566ab3b68ff15d4b917
Sophie <sophie.sattler@ifmpt.de>
2019-01-28 16:32:37 +0100

    Strip plot Script angepasst

--- py_scripts/strip_plot.py
+++ py_scripts/strip_plot.py
@@ -5,13 +5,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 p_value_table = pd.read_csv(snakemake.input[0], sep='\t')
-#p_value_table = pd.read_csv('p-values_all_transcripts.csv', sep='\t')
+#p_value_table = pd.read_csv('significant_transcripts.csv', sep='\t')
 
-#print("tabelle: ")
-#print(p_value_table)
+#Tabelle nach aufsteigendem p-Value sortieren
+p_value_table = p_value_table.sort_values(by=['pval'], ascending=True)
+
+#neu sortierte Indexe setzen
+p_value_table = p_value_table.reset_index(drop=True)
+
+#top20 p-Values speichern
 p_value_table = p_value_table.loc[0:20, :]
-
-#print(p_value_table)
 
 sns.set(style="white")
 #Wo finden wir die Condition?
