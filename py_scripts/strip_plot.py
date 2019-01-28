5c83ae91b2cd54da7c3698e21f43d86cef3634e6
Sophie <sophie.sattler@ifmpt.de>
2019-01-28 14:24:25 +0100

    Strip Script angepasst, erneuter Test Plots

--- py_scripts/strip_plot.py
+++ py_scripts/strip_plot.py
@@ -4,19 +4,20 @@ import seaborn as sns
 
 import matplotlib.pyplot as plt
 
-#p_value_table = pd.read_csv(snakemake.input[0], sep='\t')
-p_value_table = pd.read_csv('p-values_all_transcripts.csv', sep='\t')
+p_value_table = pd.read_csv(snakemake.input[0], sep='\t')
+#p_value_table = pd.read_csv('p-values_all_transcripts.csv', sep='\t')
 
-print("tabelle: ")
-print(p_value_table)
+#print("tabelle: ")
+#print(p_value_table)
 p_value_table = p_value_table.loc[0:20, :]
 
-print(p_value_table)
+#print(p_value_table)
+
 sns.set(style="white")
 #Wo finden wir die Condition?
 sns.stripplot(x='pval', y='ext_gene', data=p_value_table, jitter=False)
 #soll ich x und y nicht angeben?
 plt.title("Stripchart der top20 differentiell exprimierten Gene")
 
-plt.show()
-#plt.savefig(snakemake.output[0])
+#plt.show()
+plt.savefig(snakemake.output[0])
